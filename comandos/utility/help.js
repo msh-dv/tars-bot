2a3c5642b37343b99b87e13b0b741026e6795ebc
msh <mshdevv@gmail.com>
2024-07-10 23:24:09 -0600

    Add help command

---
+++ comandos/utility/help.js
@@ -0,0 +1,42 @@
+const { SlashCommandBuilder } = require("discord.js");
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("help")
+    .setDescription("Muestra informacion de ayuda."),
+  async execute(interaction) {
+    const exampleEmbed = new EmbedBuilder()
+      .setColor("Aqua")
+      .setTitle("DGPT Bot")
+      .setURL("https://discord.js.org/")
+      .setAuthor({
+        name: "msh",
+        iconURL: "https://i.imgur.com/AfFp7pu.png",
+        url: "https://discord.js.org",
+      })
+      .setDescription("Some description here")
+      .setThumbnail("https://i.imgur.com/AfFp7pu.png")
+      .addFields(
+        { name: "Regular field title", value: "Some value here" },
+        { name: "\u200B", value: "\u200B" },
+        { name: "Inline field title", value: "Some value here", inline: true },
+        { name: "Inline field title", value: "Some value here", inline: true }
+      )
+      .addFields({
+        name: "Inline field title",
+        value: "Some value here",
+        inline: true,
+      })
+      .setImage("https://i.imgur.com/AfFp7pu.png")
+      .setTimestamp()
+      .setFooter({
+        text: "Some footer text here",
+        iconURL: "https://i.imgur.com/AfFp7pu.png",
+      });
+
+    // channel.send({ embeds: [exampleEmbed] });
+
+    await interaction.reply({embeds:[exampleEmbed]});
+  },
+};
