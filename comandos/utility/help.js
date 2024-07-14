82983d802beb0a206d70c9867876e29f871468dd
msh <mshdevv@gmail.com>
2024-07-13 22:54:52 -0600

    Add a help command

--- comandos/utility/help.js
+++ comandos/utility/help.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, codeBlock, bold } = require("discord.js");
 const { EmbedBuilder } = require("discord.js");
 
 module.exports = {
@@ -7,36 +7,31 @@ module.exports = {
     .setDescription("Muestra informacion de ayuda."),
   async execute(interaction) {
     const exampleEmbed = new EmbedBuilder()
-      .setColor("Aqua")
-      .setTitle("DGPT Bot")
-      .setURL("https://discord.js.org/")
-      .setAuthor({
-        name: "msh",
-        iconURL: "https://i.imgur.com/AfFp7pu.png",
-        url: "https://discord.js.org",
-      })
-      .setDescription("Some description here")
-      .setThumbnail("https://i.imgur.com/AfFp7pu.png")
-      .addFields(
-        { name: "Regular field title", value: "Some value here" },
-        { name: "\u200B", value: "\u200B" },
-        { name: "Inline field title", value: "Some value here", inline: true },
-        { name: "Inline field title", value: "Some value here", inline: true }
+      .setColor("White")
+      .setTitle("Comandos")
+      .setDescription(bold("Información sobre comandos:"))
+      .setThumbnail(
+        "https://msh-dv.github.io/tars-website/images/tars-profile.png"
       )
-      .addFields({
-        name: "Inline field title",
-        value: "Some value here",
-        inline: true,
-      })
-      .setImage("https://i.imgur.com/AfFp7pu.png")
-      .setTimestamp()
-      .setFooter({
-        text: "Some footer text here",
-        iconURL: "https://i.imgur.com/AfFp7pu.png",
-      });
+      .addFields(
+        {
+          name: bold("/chat"),
+          value: codeBlock("Envia tu mensaje a ChatGPT."),
+        },
+        {
+          name: bold("/server"),
+          value: codeBlock("Muestra información del servidor."),
+        },
+        {
+          name: bold("/info"),
+          value: codeBlock("Muestra información del usuario."),
+        },
+        { name: bold("/hi"), value: codeBlock("Responde con un Hola!") },
+        { name: bold("/echo"), value: codeBlock("Repite lo que le digas.") },
+        { name: bold("/ping"), value: codeBlock("Responde con un Pong!") }
+      )
+      .setTimestamp();
 
-    // channel.send({ embeds: [exampleEmbed] });
-
-    await interaction.reply({embeds:[exampleEmbed]});
+    await interaction.reply({ embeds: [exampleEmbed] });
   },
 };
