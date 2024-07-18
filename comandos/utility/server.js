8389e0ca2088fb6d65b021e5fb2e9830c9b6312c
msh <mshdevv@gmail.com>
2024-07-17 23:23:24 -0600

    Update server command

--- comandos/utility/server.js
+++ comandos/utility/server.js
@@ -18,3 +18,45 @@ module.exports = {
     );
   },
 };
+
+const { SlashCommandBuilder, codeBlock, bold } = require("discord.js");
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("help")
+    .setDescription("Muestra informacion de ayuda."),
+  async execute(interaction) {
+    const exampleEmbed = new EmbedBuilder()
+      .setColor("White")
+      .setTitle(`${i.name} (${i.id})`)
+      .setDescription(bold("Información del servidor:"))
+      .setThumbnail(
+        "https://msh-dv.github.io/tars-website/images/tars-profile.png"
+      )
+      .addFields(
+        {
+	  name: bold("Usuarios:"),
+          value: `${i.memberCount}`,
+        },
+        {
+	  name: bold("Creado desde:"),
+          value: "Envia un mensaje privado a ChatGPT.",
+        },
+        {
+          name: bold("/server"),
+          value: "Muestra información del servidor.",
+        },
+        {
+          name: bold("/info"),
+          value: codeBlock("Muestra información del usuario."),
+        },
+        { name: bold("/hi"), value: codeBlock("Responde con un Hola!") },
+        { name: bold("/echo"), value: codeBlock("Repite lo que le digas.") },
+        { name: bold("/ping"), value: codeBlock("Responde con un Pong!") }
+      )
+      .setTimestamp();
+
+    await interaction.reply({ embeds: [exampleEmbed] });
+  },
+};
