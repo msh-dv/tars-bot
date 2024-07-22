9677bbe69a43f7670629d4a52f2b974d9da2bf58
msh <mshdevv@gmail.com>
2024-07-21 22:25:59 -0600

    Updated priv command

--- comandos/utility/priv.js
+++ comandos/utility/priv.js
@@ -25,12 +25,11 @@ module.exports = {
       stream: true,
     });
 
-    console.log("Nueva instancia de IA creada");
     const chatCompletion = await stream.finalChatCompletion();
     const finalMessage = chatCompletion.choices[0].message.content;
     await interaction.editReply(`${finalMessage}`);
     console.log(
-      `\x1b[1;32mPublic: \x1b[1;34m${inte.user.username}\x1b[0m at ${inte.createdAt}\x1b[0m
+      `\x1b[1;31mPrivate: \x1b[1;34m${inte.user.username}\x1b[0m at ${inte.createdAt}\x1b[0m
       Message: ${mensaje}
       Respones: ${finalMessage} `
     );
