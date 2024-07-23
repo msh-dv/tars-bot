e27224965eee1f1415be83474cfc4d1d05677d41
msh <mshdevv@gmail.com>
2024-07-23 15:13:27 -0600

    Fix key bug

--- comandos/utility/chat.js
+++ comandos/utility/chat.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require("discord.js");
 require("dotenv").config();
 
 const openai = new OpenAI({
-  apiKey: process.env[OPENAI_API_KEY],
+  apiKey: process.env['OPENAI_API_KEY'],
 });
 
 module.exports = {
