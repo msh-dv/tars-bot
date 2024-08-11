d49aa58c31e2689dd7a0c22a9bf004358882e7fe
msh <mshdevv@gmail.com>
2024-08-11 02:12:29 -0600

    Fix permissions invitation link

--- comandos/utility/invite.js
+++ comandos/utility/invite.js
@@ -10,7 +10,7 @@ module.exports = {
       .setColor("White")
       .setTitle("Link de invitacion")
       .setDescription(
-        "[Haz click para invitarme a tu servidor.](https://discord.com/oauth2/authorize?client_id=1260411192640409600&permissions=28445502074103&integration_type=0&scope=bot+applications.commands)"
+        "[Haz click para invitarme a tu servidor.](https://discord.com/oauth2/authorize?client_id=1260411192640409600&permissions=1129183410388032&integration_type=0&scope=bot+applications.commands)"
       )
       .setTimestamp();
 
