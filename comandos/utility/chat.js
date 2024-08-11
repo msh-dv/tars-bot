644c8b210049be2dfe5c33350350a3239ffd8dda
msh <mshdevv@gmail.com>
2024-08-11 05:50:47 -0600

    Update chat command

--- comandos/utility/chat.js
+++ comandos/utility/chat.js
@@ -27,34 +27,38 @@ module.exports = {
     const mensaje = interaction.options.getString("mensaje");
 
     //instrucciones predeterminadas
-    const instrucciones = `
-    Instrucciones:
-    Eres TARS un bot de discord que usa la API de OpenAI para dar respuestas generadas con IA.
-    Tu nombre hace referencia al robot de la pelicula interestelar TARS.
-    Idioma de la respuesta: El mismo que el mensaje del usuario.
-    Nombre del usuario:${inte.member.displayName}.
-    Longitud de respuestas: medias.
-    Tipo de respuestas: formales, detalladas.
+    const instrucciones =
+      `Eres TARS, un bot de discord que usa los modelos de OpenAI para dar respuestas creativas y detalladas de cualquier tema. The user's language should be the same as the language of the user's input. Recuerda saludar con el nombre del usuario, el cual es ${inte.member.displayName}.`;
 
-    A continuacion, responde al mensaje del usuario:
-    `;
-
-    const stream = await openai.beta.chat.completions.stream({
+    const completion = await openai.chat.completions.create({
       //Agregamos la informacion para hacer la peticion a la API de OpenAI
+      messages: [
+        {
+          role: "system",
+          content: instrucciones.trim(),
+        },
+        { role: "user", content: mensaje },
+      ],
       model: "gpt-4o-mini",
-      messages: [{ role: "user", content: instrucciones + mensaje }],
-      stream: true,
+      max_tokens: 600,
     });
     //Creamos una variable para almacenar la respuesta completa
-    const chatCompletion = await stream.finalChatCompletion();
-    const finalMessage = chatCompletion.choices[0].message.content;
-    //Redirigimos la respuesta y la editamos para añadir nuestra respuesta
-    await interaction.editReply(`${finalMessage}`);
+    const chatCompletion = completion.choices[0].message.content;
+
+    if (chatCompletion.length > 2000) {
+      const firstPart = chatCompletion.substring(0, 2000);
+      const secondPart = chatCompletion.substring(2000);
+      await interaction.editReply(`${firstPart}`);
+      await interaction.followUp(`${secondPart}`);
+    } else {
+      await interaction.editReply(`${chatCompletion}`);
+    }
     //Logs de las interacciones
     console.log(
       `Public: ${inte.user.username} at ${inte.createdAt}
       Message: ${mensaje}
-      Response: ${finalMessage} 
+      Response: ${chatCompletion}, Response.length = ${chatCompletion.length}
+      Tokens: ${completion.usage.total_tokens}
       `
     );
   },
