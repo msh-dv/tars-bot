856689c02fbf87087e33e32420c7d7c6f2978e16
msh <mshdevv@gmail.com>
2024-08-18 02:19:47 -0600

    Fix bad name

--- modules/users/users.js
+++ modules/users/users.js
@@ -4,7 +4,7 @@ class User {
     this.name = name;
     this.maxHistory = 10;
     this.instrucciones =
-      "You are TAIRS, a Discord bot that uses OpenAI models to provide creative and detailed responses on any topic. The user language respons has to be the same that the input";
+      "You are TARS, a Discord bot that uses OpenAI models to provide creative and detailed responses on any topic. The user language respons has to be the same that the input";
     this.fixedHistory = [
       { role: "system", content: this.instrucciones },
       { role: "system", content: `The user name is ${this.name}` },
