2c71c52e62e0c617b3dffb295ea0b0a653641e9d
msh <mshdevv@gmail.com>
2024-08-22 18:48:44 -0600

    Add new username setter in class User

--- modules/users/users.js
+++ modules/users/users.js
@@ -2,10 +2,14 @@ class User {
   constructor(id, name) {
     this.id = id;
     this.name = name;
+<<<<<<< HEAD
     this.TextModel = "gpt-4o-mini";
     this.ImageModel = "dall-e-2";
     this.AudioModel = "tts-1";
     this.maxHistory = 10;
+=======
+    this.maxHistory = 12;
+>>>>>>> 89d19e9 (Add new username setter in class User)
     this.instrucciones =
       "You are TARS, a Discord bot that uses OpenAI models to provide creative and detailed responses on any topic. The user language respons has to be the same that the input";
     this.fixedHistory = [
@@ -13,6 +17,10 @@ class User {
       { role: "system", content: `The user name is ${this.name}` },
     ];
     this.dynamicHistory = [];
+
+    this.isPremium = false;
+    this.isBanned = false;
+    this.tokens = 1000;
   }
 
   addMessage({ role, content }) {
@@ -30,6 +38,14 @@ class User {
   setNewInstructions(newInstructions) {
     this.instrucciones = newInstructions;
     this.fixedHistory[0] = { role: "system", content: newInstructions };
+  }
+
+  setNewUsername(newUsername) {
+    this.name = newUsername;
+    this.fixedHistory[1] = {
+      role: "system",
+      content: `The user name is ${newUsername}`,
+    };
   }
 }
 
