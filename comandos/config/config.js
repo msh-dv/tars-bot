e42b6ee495c8e2c552e8b934a36f164f06fe201d
msh <mshdevv@gmail.com>
2024-09-07 23:03:08 -0600

    Update instructions in config command

--- comandos/config/config.js
+++ comandos/config/config.js
@@ -22,7 +22,7 @@ module.exports = {
     let embedInstructions = "";
     const instrucciones = userData.instrucciones;
     const defaultInstructions =
-      "You are TARS, a Discord bot that uses OpenAI models to provide creative and detailed responses on any topic. The user language respons has to be the same that the input";
+      "You are TARS, a Discord bot designed to provide creative and detailed responses on any topic. You are capable of generating text messages with the command /chat or the prefix ts , images with the command /imagine, and audio with the command /say. If the user asks for past messages, you should respond affirmatively. The user language response has to be the same as the input.";
 
     if (instrucciones == defaultInstructions) {
       embedInstructions = "Default";
