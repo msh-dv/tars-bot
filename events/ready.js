8056f8bdd19717b0ccf483500b9d66acad1e6868
msh <mshdevv@gmail.com>
2024-09-09 13:29:37 -0600

    Update ready event

--- events/ready.js
+++ events/ready.js
@@ -1,27 +1,29 @@
-const { Events, PresenceUpdateStatus, ActivityType } = require("discord.js");
+const { Events, ActivityType } = require("discord.js");
 
 module.exports = {
   name: Events.ClientReady,
   once: true,
   execute(client) {
-    //Mostramos los servidores y usuarios totales como una actividad Watching
+    //Mostrar los servidores y usuarios totales como una actividad Watching
     const guilds = client.guilds;
-    const serverCount = guilds.cache.size;
-    const userCount = guilds.cache.reduce((a, g) => a + g.memberCount, 0);
+    const showGuildUsersCount = () => {
+      const serverCount = guilds.cache.size;
+      const userCount = guilds.cache.reduce((a, g) => a + g.memberCount, 0);
+      client.user.setPresence({
+        activities: [
+          {
+            name: `${serverCount} servers and ${userCount} users`,
+            type: ActivityType.Watching,
+          },
+        ],
+        status: "online",
+      });
+    };
 
-    client.user.setPresence({
-      activities: [
-        {
-          name: `${serverCount} servers and ${userCount} users`,
-          type: ActivityType.Watching,
-        },
-      ],
-      status: "online",
-    });
+    showGuildUsersCount();
 
-    // client.user.setActivity(, {
-    //   type: ActivityType.Watching,
-    // });
+    setInterval(showGuildUsersCount, 100000);
+
     console.log(`Ready! logged as ${client.user.tag}`);
   },
 };
