a00bbe05c205bd491fabd6c5b839019d23c94a98
msh <mshdevv@gmail.com>
2024-09-09 18:29:03 -0600

    Update prefix structure

--- events/prefix.js
+++ events/prefix.js
@@ -6,62 +6,72 @@ module.exports = {
   name: "messageCreate",
   once: false,
   async execute(message) {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-    const content = message.content.slice(prefix.length).trim();
-    const attachmentURL = message.attachments.first();
-    const userID = message.author.id;
-    const userName = message.author.displayName;
-    const date = message.createdAt;
+    const { content, attachments, author, createdAt, channel, reference } =
+      message;
+    const { id: userID, displayName: userName, bot: isBot } = author;
+    contentLower = content.toLowerCase();
 
-    // TODO:Agregar una validacion para el id y nombre
+    if (!contentLower.startsWith(prefix) || isBot) return;
+
+    const command = content.slice(prefix.length).trim();
+    const attachment = attachments.first();
+    const logMessage = `${createdAt}\nPublico (prefijo): ${userName} ${userID}\nmsg: ${command}`;
+
+    // Función para enviar mensajes largos
+    const sendLongMessage = async (msg) => {
+      if (msg.length > 2000) {
+        await channel.send(msg.slice(0, 2000));
+        await channel.send(msg.slice(2000));
+      } else {
+        await channel.send(msg);
+      }
+    };
+
+    // Manejo de errores
+    const handleError = (err) => {
+      console.error("Error de comando (prefijo):", err.message);
+      message.reply("> *Hubo un error ejecutando este comando.*");
+    };
+
+    // Manejo de mensajes referenciados
+    let referencedMessageContent = null;
+    if (reference) {
+      try {
+        const referencedMessage = await channel.messages.fetch(
+          reference.messageId
+        );
+        referencedMessageContent = referencedMessage.content;
+        console.log(`Mensaje referenciado: ${referencedMessageContent}`);
+      } catch (err) {
+        console.error("Error al obtener el mensaje referenciado:", err.message);
+      }
+    }
 
     try {
-      console.log(
-        `${date}\nPublico (prefijo): ${userName} ${userID}\nmsg: ${content}`
-      );
+      console.log(logMessage);
 
-      if (attachmentURL) {
-        console.log(
-          `${date}\nPublico (prefijo): ${userName} ${userID}\nmsg: ${content}\n${attachmentURL.url}`
-        );
+      if (attachment) {
+        console.log(`${logMessage}\n${attachment.url}`);
         const imgResponse = await imageVision(
           userID,
           userName,
-          content,
-          attachmentURL.url
+          command,
+          attachment.url
         );
-        if (imgResponse) {
-          message.channel.send(imgResponse);
-        } else {
-          message
-            .reply("> *Hubo un error ejecutando este comando.*")
-            .catch((err) => console.error(err));
-        }
+        imgResponse
+          ? await sendLongMessage(imgResponse)
+          : handleError(new Error("Error procesando la imagen"));
       } else {
-        const res = await textReq(userID, userName, content);
-
-        if (res) {
-          if (res.length > 2000) {
-            const firstPart = res.substring(0, 2000);
-            const secondPart = res.substring(2000);
-
-            message.channel.send(firstPart);
-            message.channel.send(secondPart);
-          } else {
-            message.channel.send(res);
-          }
-        } else {
-          message
-            .reply(
-              `> *This message violates our usage policies.*
-      > *Este mensaje inflige nuestras politicas de uso.*`
-            )
-            .catch((err) => console.error(err));
-        }
+        const finalCommand = referencedMessageContent
+          ? `${referencedMessageContent} ${command}`
+          : command;
+        const textResponse = await textReq(userID, userName, finalCommand);
+        textResponse
+          ? await sendLongMessage(textResponse)
+          : handleError(new Error("Error procesando el texto"));
       }
     } catch (err) {
-      console.error("Error de comando (prefijo):", err.message);
-      message.reply("> *Hubo un error ejecutando este comando.*");
+      handleError(err);
     }
   },
 };
